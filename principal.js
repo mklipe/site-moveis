964deaba8d388852377b5267f507cce54981a616
Mauricio Klipe <mklipe@yahoo.com.br>
2018-03-01 21:21:56 -0400

    Adicionando banner js

--- principal.js
+++ principal.js
@@ -5,7 +5,7 @@ var bannerAtual = 0;
 
 function trocaBanner() {
 	bannerAtual = (bannerAtual + 1) % 2;
-	document.querySelector(’h2#mensagem’).textContent =
+	document.querySelector('h2#mensagem').textContent =
 	banners[bannerAtual];
 }
 
